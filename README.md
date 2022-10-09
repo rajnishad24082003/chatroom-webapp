192241fb26b7f22d8f4e80fc3d2b2bee15a8fb4a
[raj nishad] <[rajnishad24082003@gmail.com]>
2022-10-09 19:08:55 +0530

    added readme

---
+++ README.md
@@ -0,0 +1,28 @@
+# discription
+this website contains a chatroom in which anybody can text, after sigin.
+
+# fullstack website
+this is a complete website with frontend and backend.
+for frontend i have use html/css/js/hbs.
+for backend it is node.js/express.
+data base is of mongodb.
+for authentication i have used google oauth 2.0 policy form passport.js.
+
+# getting started
+1) npm install in terminal.
+2)create a file named config.env inside configFolder.
+3)copy the text:-
+
+MONGO_URI = your mongodb uri
+
+GOOGLE_CLIENT_ID = your google client id
+
+GOOGLE_CLIENT_SECRET = your google secrect
+
+4)npm start
+
+# after npm start
+1) entry point of this website is app.js.
+2)once start you will see:-
+server listening on port 3000
+connected to mongodb at cluster......
--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require("express");
-
 const dotenv = require("dotenv");
 const path = require("path");
 const MongoStore = require("connect-mongo");
@@ -9,21 +8,28 @@ dotenv.config({path:"./configFolder/config.env"});
 const  exphbs =  require('express-handlebars');
 
 //passport
-
 const passport = require("passport");
 require("./configFolder/passport")(passport);
 //passport
+
+
 //database
 const connected = require("./configFolder/database");
 const connectToDataBase = require("./configFolder/database");
 //database
-const app = express();
-//dotenv
 
+
+const app = express();
+
+
+//dotenv
 connectToDataBase();
 //dotenv
+
+
 app.use(express.urlencoded({extended:false}));
 app.use(express.static("./public"));
+
 
 const isloggedin = (req,res,next)=>{
 if(req.isAuthenticated())
@@ -35,6 +41,8 @@ else
     res.sendStatus(401);
 }
 }
+
+
 app.use(session({secret:"cats",
 resave:false,
 saveUninitialized:false,
@@ -42,8 +50,11 @@ store: MongoStore.create({
     mongoUrl: process.env.MONGO_URI
 })
 }));
+
+
 app.use(passport.initialize());
 app.use(passport.session())
+
 
 app.get("/auth/google",(req,res,next)=>{
     if(req.isAuthenticated())
@@ -60,24 +71,14 @@ app.set("view engine",".hbs");
 app.get("/google/callback",passport.authenticate("google",{failureRedirect:"/components/signin"}),(req,res)=>{
    res.redirect("/on_signin_pages/storiesPage/user_typed_data");
 })
-//app.get("/on_signin_pages/storiesPage",isloggedin,(req,res)=>{
-//res.sendFile("/on_signin_pages/storiesPage.html",{root:path.join(__dirname)})
-//});
-//app.use("/",require("./folder_for_dashboard/require_dashboard"));
+
+
 app.get("/components/sigin",(req,res)=>{
     res.sendFile("/public/components/signin.html",{root:path.join(__dirname)})
 })
 app.use("/on_signin_pages/storiesPage/user_typed_data",require("./folder_for_dashboard/routerforuserdata"));
-//app.use("/on_signin_pages/storiesPage/",task);
-//app.post("/on_signin_pages/storiesPage",async (req,res)=>{
-//   try {
-//        const title = req.body.title;
-//     console.log(`${title}`);
-//        res.send("post requirest sucessful")
-//    } catch (error) {
-//     console.log(error);
- //   }  
-//});
+
+
 app.get("/logout",(req,res)=>{
     req.logout(function(err) {
         if (err) { return next(err);}
@@ -85,6 +86,7 @@ app.get("/logout",(req,res)=>{
         })
     })
 
+    
 app.listen(3000,()=>{
     console.log("server listening on port 3000");
 })
